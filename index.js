20276794aeed97b579395d7ee64d5a657abc14f3
Inna <kubasovainna@mail.ru>
2022-01-12 03:13:25 +0300

    docs: add readme

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ message: 'Hello WWW!' });
-});
+app.set('view engine', 'ejs')
+
+app.use('/', function (request, response) {
+	response.render('main', {
+		title: 'Список терминов',
+		terms: terms
+	})
+})
 
 app.listen(3333, () => {
 	console.log('Application listening on port 3333!');
